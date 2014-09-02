66c79b5fdcf53fc912cd3607ec326b727aa96168
Igor Fedorenko <igor@ifedorenko.com>
2014-09-02 13:16:28 -0400

    updated to match recent maven executor changes
    Signed-off-by: Igor Fedorenko <igor@ifedorenko.com>

--- maven-extension/src/test/java/sample/extension/tests/it/MavenExtensionIntegrationTest.java
+++ maven-extension/src/test/java/sample/extension/tests/it/MavenExtensionIntegrationTest.java
@@ -3,46 +3,40 @@ package sample.extension.tests.it;
 import io.takari.maven.testing.TestDependencies;
 import io.takari.maven.testing.TestProperties;
 import io.takari.maven.testing.TestResources;
-import io.takari.maven.testing.it.MavenVersions;
-import io.takari.maven.testing.it.VerifierResult;
-import io.takari.maven.testing.it.VerifierRuntime;
+import io.takari.maven.testing.executor.MavenExecutionResult;
+import io.takari.maven.testing.executor.MavenRuntime;
+import io.takari.maven.testing.executor.MavenRuntime.MavenRuntimeBuilder;
+import io.takari.maven.testing.executor.MavenVersions;
+import io.takari.maven.testing.executor.junit.MavenJUnitTestRunner;
 
 import java.io.File;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-import org.junit.runners.Parameterized.Parameters;
 
-@RunWith(Parameterized.class)
+@RunWith(MavenJUnitTestRunner.class)
+@MavenVersions({"3.2.1", "3.2.2"})
 public class MavenExtensionIntegrationTest {
 
   @Rule
-  public final TestResources resources;
-
-  public final VerifierRuntime verifier;
+  public final TestResources resources = new TestResources();
 
   public final TestProperties proprties = new TestProperties();
 
   public final TestDependencies dependencies = new TestDependencies(proprties);
 
-  @Parameters(name = "maven-{0}")
-  public static Iterable<Object[]> mavenVersions() {
-    return MavenVersions.asJunitParameters("3.2.1", "3.2.2");
-  }
+  public final MavenRuntime verifier;
 
-  public MavenExtensionIntegrationTest(String mavenVersion) throws Exception {
-    this.resources = new TestResources("src/test/projects", "target/it/" + mavenVersion + "/");
-    this.verifier = VerifierRuntime.builder(mavenVersion) //
-        .withExtensions(dependencies.getRuntimeClasspath()) //
+  public MavenExtensionIntegrationTest(MavenRuntimeBuilder runtimeBuilder) throws Exception {
+    this.verifier = runtimeBuilder.withExtensions(dependencies.getRuntimeClasspath()) //
         .build();
   }
 
   @Test
   public void testBasic() throws Exception {
     File basedir = resources.getBasedir("basic-it");
-    VerifierResult result = verifier.forProject(basedir) //
+    MavenExecutionResult result = verifier.forProject(basedir) //
         .withCliOption("-X") //
         .execute("package");
     result.assertErrorFreeLog();
